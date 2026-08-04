c1c490da9158a2fdc8e64adbcaf3e8bde7953ca0
unknown <akshaykrish533516@gmail.com>
2026-08-04 20:02:21 +0530

    Implemented Resume Download Feature in About Me Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -21,9 +21,10 @@ const About = () => {
 
           I specialize in <span className='text-white'>React, Next.js, Node.js, Express, PostgreSQL, MongoDB, and TypeScript</span>. I enjoy building scalable applications with clean architecture, maintainable code, and a strong emphasis on performance, security, and user experience.</p>
         </div>
-        <div className='mt-8 flex justify-center items-center gap-4'>
+        <div className='mt-8 flex flex-wrap justify-center items-center gap-4'>
           <Link href='mailto:akshaykrish533516@gmail.com' className='bg-cyan-400 rounded-3xl px-4 py-2'>Get in Touch</Link>
           <Link href="https://www.linkedin.com/in/akshay-krish/" className="text-white border border-white px-4 py-2 text-center flex flex-wrap justify-center gap-2 items-center rounded-4xl"><svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path></svg>LinkedIn</Link>
+          <Link href='/Akshay_Krishnan_FullStack_Resume.pdf' download='Akshay_Krishnan_Resume.pdf' className='bg-cyan-400 rounded-3xl px-4 py-2'>⬇ Download Resume</Link>
         </div>
       </main>
     </div>
